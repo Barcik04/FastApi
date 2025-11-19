fcafac347d6c662dac6a9832eb1d334dcf930e26
Igor Barcikowski <igor.bb00@gmail.com>
2025-11-19 13:43:56 +0100

    V29

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=False,
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
     allow_methods=["*"],
     allow_headers=["*"],
 )
