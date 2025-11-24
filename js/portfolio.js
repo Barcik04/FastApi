2ae53233e56770a594fa9700b55df9581b7587d4
Igor Barcikowski <Igor.bb00@gmail.com>
2025-11-24 12:31:18 +0100

    V4

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,11 +15,36 @@ showPnlBtn.addEventListener('click', async() => {
         });
 
         if (response.status === 200) {
-            resultDiv.textContent = JSON.stringify(response.data, null, 2);
-            resultDiv.style.whiteSpace = "pre-wrap"
+            const data = response.data;
+
+            document.getElementById('portfolioId').textContent = `ID: ${data.id}`;
+            document.getElementById('coins').textContent = `Coins: ${JSON.stringify(data.coins)}`;
+            document.getElementById('boughtPrice').textContent = `Avg Bought Price: ${JSON.stringify(data.bought_price)}`;
+            document.getElementById('pnl').textContent = `Profit and loss: ${data.p_and_l}`;
         }
     } catch (error) {
         resultDiv.textContent = "Error loading portfolio.";
         console.log(error);
     }
+
+    try {
+        const graphResponse = await axios.get('/transactions/p_n_l', {
+            headers: {
+                Authorization: `Bearer ${jwt}`
+            },
+            responseType: 'arraybuffer'
+        });
+
+        if (graphResponse.status === 200) {
+            const blob = new Blob([graphResponse.data], { type: 'image/png' });
+            const url = URL.createObjectURL(blob);
+
+            const img = document.getElementById('p_n_l_graph');
+            img.src = url;
+        }
+    } catch (error) {
+        resultDiv.textContent = "Error loading p_n_l_graph.";
+        console.log(error);
+    }
+
 })
